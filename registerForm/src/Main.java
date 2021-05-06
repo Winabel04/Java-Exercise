9d8557da3c23f42af0366f1c87b76830d6b1594f
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-05-06 20:38:21 +0800

    sir mao ni ang imong activity nga CRUD Jframe in java

---
+++ registerForm/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    
+        AddFrame main = new AddFrame();
+        main.setVisible(true);
+
+//
+//    ShowFrame show = new ShowFrame();
+//    show.setVisible(true);
+ }
+    
+}
